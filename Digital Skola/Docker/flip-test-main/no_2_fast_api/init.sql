a5fbc6fb0f9100f08ca09dd4423b105a13acfc93
Ezra <127650295+Ghifariezra@users.noreply.github.com>
2024-05-26 20:15:55 +0700

    Delete Digital Skola/Docker directory

--- Digital Skola/Docker/flip-test-main/no_2_fast_api/init.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE pokemon_effect(
-   id SERIAL PRIMARY KEY,
-   loan_id BIGINT,
-   user_id BIGINT,
-   pokemon_ability_id INTEGER,
-   effect VARCHAR,
-   language VARCHAR,
-   short_effect VARCHAR
-);
