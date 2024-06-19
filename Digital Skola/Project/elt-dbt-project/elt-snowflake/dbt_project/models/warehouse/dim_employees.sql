3ae98e53ac676b84db50ba889bb25ab2ca9e70a6
Ezra <127650295+Ghifariezra@users.noreply.github.com>
2024-06-19 19:32:34 +0700

    Delete Digital Skola/Project/elt-dbt-project directory

--- Digital Skola/Project/elt-dbt-project/elt-snowflake/dbt_project/models/warehouse/dim_employees.sql
+++
@@ -1,5 +0,0 @@
-WITH raw_employees AS (
-    SELECT * FROM {{ ref('raw_employees') }}
-)
-
-SELECT * FROM raw_employees
--- Digital Skola/Project/elt-dbt-project/learn-dbt/dbt_dskola_project/models/staging/raw_products.sql
+++
@@ -1,6 +0,0 @@
-select
-    product_id,
-    product_name,
-    category
-from
-    {{ source('raw','products') }}
